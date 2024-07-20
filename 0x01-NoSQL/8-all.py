a66d3df1beac2e3156e2086106e5102fc42a2869
MicrQ <mail.micrq@proton.me>
2024-07-20 09:10:36 +0300

    modified

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ pymongo project task """
-from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
     """ returns all documents in a collection """
-    return mongo_collection.find()
+    docs = mongo_collection.find()
+    return docs if docs.count() > 0 else []
