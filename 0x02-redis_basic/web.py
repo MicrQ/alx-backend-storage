4d493076ffd7bcdc87cf6b026f159509df5d54d0
AbenetGebre <mail.micrq@proton.me>
2024-08-29 22:23:24 +0300

    defined decorator first

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,22 +8,6 @@ from typing import Callable
 
 cache = redis.Redis()
 
-
-@count_calls
-def get_page(url: str) -> str:
-    """ obtains HTML content of a particular URL
-    Return:
-        The HTML content
-    """
-    res = cache.get(url)
-    if res:
-        return res
-    else:
-        res = requests.get(url).text
-        cache.setex(url, 10, res)
-    return res
-
-
 def count_calls(fn: Callable) -> Callable:
     """ counts how many time a function is called """
     @wraps(fn)
@@ -34,3 +18,18 @@ def count_calls(fn: Callable) -> Callable:
         result = fn(*args, **kwargs)
         return result
     return wrapper
+
+
+@count_calls
+def get_page(url: str) -> str:
+    """ obtains HTML content of a particular URL
+    Return:
+        The HTML content
+    """
+    res = cache.get(url)
+    if res:
+        return res.decode('utf-8')
+    else:
+        res = requests.get(url).text
+        cache.setex(url, 10, res)
+    return res
