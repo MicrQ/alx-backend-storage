a7d6951149ed6f251a62df854e9b131be634167f
AbenetGebre <mail.micrq@proton.me>
2024-08-29 21:49:20 +0300

    implemented wrapper to count the number of time a function is called in 10 seconds

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,11 +2,14 @@
 """ Implementing an expiring web cache and tracker """
 import requests
 import redis
+from functools import wraps
+from typing import Callable
 
 
 cache = redis.Redis()
 
 
+@count_calls
 def get_page(url: str) -> str:
     """ obtains HTML content of a particular URL
     Return:
@@ -14,3 +17,14 @@ def get_page(url: str) -> str:
     """
     cache.setex('count:' + url, 10)
     return requests.get(url).text
+
+def count_calls(fn: Callable) -> Callable:
+    """ counts how many time a function is called """
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        """ moke function """
+        key = 'count:' + args[0]
+        cache.incr(key)
+        cache.setex(key, 10)
+        return fn(*args, **kwargs)
+    return wrapper
